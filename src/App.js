1420ab2bfa9c83da0ce9d43eb8c0f1e4c8533bd8
Amir Jaman Mondal <85990039+amirjamanmondal@users.noreply.github.com>
2023-09-19 02:14:54 +0530

    create navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-
-import Restaurent from "./components/Restaurent";
 import "bootstrap/dist/css/bootstrap.css";
+
+import NavBar from "./components/NavBar";
+import Restaurent from "./components/Restaurent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <div class="container-fluid">
         <Restaurent />
       </div>
+      <Footer/>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return ( <>
-    {/*  */}
-    </> );
+  return (
+    <footer class="bg-light text-center text-lg-start">
+      <div
+        class="text-center p-3"
+        style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}
+      >
+        © 2020 Copyright:
+          Restaurent App
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
